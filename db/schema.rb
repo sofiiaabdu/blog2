e23dd10f3d3ac1425e4b26f1527490b6b1e06132
Tetiana Zaskorodnia <tetianazask@tahometer.com>
2018-08-30 00:27:11 +0300

    Create blogs table in the database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_130840) do
+ActiveRecord::Schema.define(version: 2018_08_29_205340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
