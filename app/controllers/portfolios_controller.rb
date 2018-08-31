d0cd9a8b737e3e88d315f96b0d869f9c6269656c
Tetiana Zaskorodnia <tetianazask@tahometer.com>
2018-08-31 14:31:49 +0300

    Add destroy action for portfolip feature.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,7 +40,15 @@ class PortfoliosController < ApplicationController
 
   def show
     @portfolio_item = Portfolio.find(params[:id])
+  end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'The record was successfully deleted.' }
+      format.json { head :no_content }
+    end
   end
 
 end
