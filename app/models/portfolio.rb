4407314a3772c6c5fca8fa399cfa9bfa9f6289a7
sofiathepenguin <sofiia>
2018-09-03 17:25:23 +0300

    Updated routes.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
   def self.rails
     where(subtitle: 'Ruby on Rails')
   end
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://via.placeholder.com/600x400"
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
+  end
 end
